374c3b46634ac72c7d7cb00d1449cce2d1d3dd5a
Nele Smeets <nele_smeets@peopleware.be>
2005-04-05 08:40:21 +0000

    Added test cases for Period.
    git-svn-id: file:///repositories/PPWCode/trunk/ppw-value@351 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/java/be/peopleware/value_I/Cases.java
+++ src/java/be/peopleware/value_I/Cases.java
@@ -1,11 +1,17 @@
 package be.peopleware.value_I;
 
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.toryt.TorytException;
 import org.toryt.support.straightlist.ArrayStraightList;
+import org.toryt.support.straightlist.NullFirstStraightList;
 import org.toryt.support.straightlist.StraightList;
 
 import be.peopleware.bean_IV.PropertyException;
-import be.peopleware.value_I.NationalNumber;
 
 
 /**
@@ -51,6 +57,55 @@ public final class Cases {
     }
     TOL_BE_PEOPLEWARE_VALUE_I_NATIONAL_NUMBER = new ArrayStraightList(nn);
     org.toryt.Cases.addTol(NationalNumber.class, TOL_BE_PEOPLEWARE_VALUE_I_NATIONAL_NUMBER);
+  }
+
+  //  Period
+  private final static StraightList TOL_BE_PEOPLEWARE_VALUE_I_PERIOD;
+  static {
+    StraightList testValues = null;
+    try {
+      testValues = new NullFirstStraightList(
+                       org.toryt.Cases.findTestObjectList(Date.class)
+                   );
+    }
+    catch (TorytException e) {
+      assert false : "Could not load date TOL";
+    }
+    List periods = new ArrayList();
+    Iterator i1 = testValues.iterator();
+    while (i1.hasNext()) {
+      Date date1 = (Date) i1.next();
+      Iterator i2 = testValues.iterator();
+      while (i2.hasNext()) {
+        Date date2 = (Date) i2.next();
+        Period period = new Period();
+        try {
+          if (
+              date1 == null ||
+              date2 == null ||
+              ( date1 != null && date2 != null && date1.before(date2) )
+           ) {
+            period.setStartDate(date1);
+            period.setEndDate(date2);
+            periods.add(period);
+          }
+          else if (date1 != null && date2 != null && date2.before(date1)) {
+            period.setStartDate(date2);
+            period.setEndDate(date1);
+            periods.add(period);
+          }
+        }
+        catch(InvalidPeriodException exc) {
+          assert false : "Shouldn't happen";
+        }
+      }
+    }
+    Period[] array = new Period[periods.size()];
+    for(int i = 0;i<array.length;i++) {
+      array[i] = (Period)periods.get(i);
+    }
+    TOL_BE_PEOPLEWARE_VALUE_I_PERIOD = new ArrayStraightList(array);
+    org.toryt.Cases.addTol(Period.class, TOL_BE_PEOPLEWARE_VALUE_I_PERIOD);
   }
 
 }

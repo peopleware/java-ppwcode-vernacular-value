bbfdba37bfdd55cf6bb8ba30c6c819591ce48095
Jan Dockx <jan_dockx@peopleware.be>
2005-08-08 14:34:56 +0000

    New class to register PropertyEditors in PropertyEditorManager.
    git-svn-id: file:///repositories/PPWCode/trunk/ppw-value@497 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ src/java/be/peopleware/value_I/Util.java
@@ -0,0 +1,51 @@
+package be.peopleware.value_I;
+
+
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorManager;
+
+
+/**
+ * Utility class for values.
+ *
+ * @author    Jan Dockx
+ * @author    PeopleWare n.v.
+ */
+public final class Util {
+
+  /*<section name="Meta Information">*/
+  //------------------------------------------------------------------
+
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
+
+  /*</section>*/
+
+  /**
+   * No instances.
+   */
+  private Util() {
+    // NOP
+  }
+
+  /**
+   * Register packages from this library as
+   * path for {@link PropertyEditor PropertyEditors} with
+   * the {@link PropertyEditorManager}.
+   */
+  public static void registerPropertyEditors() {
+    String[] oldPath = PropertyEditorManager.getEditorSearchPath();
+    String[] newPath = new String[oldPath.length + 1];
+    System.arraycopy(oldPath, 0, newPath, 0, oldPath.length);
+    String currentPackageName = Util.class.getPackage().getName();
+    newPath[newPath.length - 1] = currentPackageName;
+    PropertyEditorManager.setEditorSearchPath(newPath);
+  }
+
+}

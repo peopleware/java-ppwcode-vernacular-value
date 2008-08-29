89c27cca21954390114ab528bb71fe851c64f0c7
Jan Dockx <jan_dockx@peopleware.be>
2008-08-29 19:17:40 +0000

    compiles
    git-svn-id: file:///repositories/PPWCode/java/vernacular/value/trunk@2453 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/value_III/EnumerationValue.java
+++ src/main/java/org/ppwcode/vernacular/value_III/EnumerationValue.java
@@ -1,9 +1,20 @@
 /*<license>
-  Copyright 2004, PeopleWare n.v.
-  NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
-  TO SELECTED PARTIES.
+Copyright 2004 - $Date$ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 </license>*/
-package be.peopleware.value_II;
+
+package org.ppwcode.vernacular.value_III;
 
 
 import java.util.Map;
@@ -191,6 +202,7 @@ public abstract class EnumerationValue extends ImmutableValue {
   /**
    * @basic
    */
+  @Override
   public final String toString() {
     return $discriminator;
   }
@@ -198,6 +210,7 @@ public abstract class EnumerationValue extends ImmutableValue {
   /**
    * @result toString().equals(other.toString());
    */
+  @Override
   public final boolean equals(final Object o) {
     return super.equals(o)
             && ($discriminator.equals(((EnumerationValue)o).$discriminator));
@@ -206,6 +219,7 @@ public abstract class EnumerationValue extends ImmutableValue {
   /**
    * @return toString().hashCode();
    */
+  @Override
   public final int hashCode() {
     return $discriminator.hashCode();
   }

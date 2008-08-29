bb79c612c555c63932be9309eb610ccd318a81c6
Jan Dockx <jan_dockx@peopleware.be>
2008-08-29 20:50:11 +0000

    prepare for jsf2
    git-svn-id: file:///repositories/PPWCode/java/vernacular/value/trunk@2488 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/value_III/jsf1/AbstractPropertyEditorConverter.java
+++ src/main/java/org/ppwcode/vernacular/value_III/jsf1/AbstractPropertyEditorConverter.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 </license>*/
 
-package org.ppwcode.vernacular.value_III.jsf;
+package org.ppwcode.vernacular.value_III.jsf1;
 
 
 import java.beans.PropertyEditor;

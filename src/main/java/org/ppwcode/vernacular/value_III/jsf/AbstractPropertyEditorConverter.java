20282fe563031b4d0b3e125559ca2a612e835e1d
Jan Dockx <jan_dockx@peopleware.be>
2008-08-29 20:52:04 +0000

    undo last change: jsf2 wil have same package name, so we need to find another solution (once we need it)
    git-svn-id: file:///repositories/PPWCode/java/vernacular/value/trunk@2490 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/value_III/jsf/AbstractPropertyEditorConverter.java
+++ src/main/java/org/ppwcode/vernacular/value_III/jsf/AbstractPropertyEditorConverter.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 </license>*/
 
-package org.ppwcode.vernacular.value_III.jsf1;
+package org.ppwcode.vernacular.value_III.jsf;
 
 
 import java.beans.PropertyEditor;

613d19c28111657ac88c6369b22a3b3a34552144
Jan Dockx <jan_dockx@peopleware.be>
2008-08-29 19:04:47 +0000

    compiles
    git-svn-id: file:///repositories/PPWCode/java/vernacular/value/trunk@2449 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/value_III/Values.java
+++ src/main/java/org/ppwcode/vernacular/value_III/Values.java
@@ -1,9 +1,21 @@
 /*<license>
-  Copyright 2004, PeopleWare n.v.
-  NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
-  TO SELECTED PARTIES.
+Copyright 2004 - $Date$ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 </license>*/
-package be.peopleware.value_II;
+
+package org.ppwcode.vernacular.value_III;
+
 
 import java.beans.PropertyEditor;
 import java.beans.PropertyEditorManager;

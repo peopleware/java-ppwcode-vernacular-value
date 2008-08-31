3ff63aa12d8e2cd8e776a1fa2c4ac68bef511e01
Jan Dockx <jan_dockx@peopleware.be>
2008-08-31 18:39:38 +0000

    cleaned
    git-svn-id: file:///repositories/PPWCode/java/vernacular/value/trunk@2518 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/value_III/ResourceBundleBasedEnumerationValueEditor.java
+++ src/main/java/org/ppwcode/vernacular/value_III/ResourceBundleBasedEnumerationValueEditor.java
@@ -17,51 +17,42 @@ limitations under the License.
 package org.ppwcode.vernacular.value_III;
 
 
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
+import static org.ppwcode.util.resourcebundle.ResourceBundles.findKeyWithBasename;
+
 import java.util.ResourceBundle;
 
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
 import org.ppwcode.util.resourcebundle.DefaultResourceBundleLoadStrategy;
 import org.ppwcode.util.resourcebundle.ResourceBundleLoadStrategy;
-import org.ppwcode.util.resourcebundle.ResourceBundles;
+import org.toryt.annotations_I.Basic;
+import org.toryt.annotations_I.Expression;
+import org.toryt.annotations_I.MethodContract;
 
 
 /**
- * <p>This class provides an implementation for an
- *   {@link EnumerationValueEditor} that returns labels for enumeration
- *   type values defined in a resource bundle, normally backed by a
- *   properties file.</p>
- * <p>The implementation expects a properties file next to this class
- *   with as name
- *   <kbd><var>getEnumerationValue().getClass().getName()</var><var>_locale_identification</var>.properties</kbd>,
- *   The <kbd><var>_locale_identification</var></kbd> should
- *   follow the traditional rules (see {@link ResourceBundle}.
+ * <p>This class provides an implementation for an {@link EnumerationValueEditor} that returns labels for enumeration
+ *   type values defined in a resource bundle, normally backed by a properties file.</p>
+ * <p>The implementation expects a properties file next to the editor class, with as name
+ *   <kbd><var>getEnumerationValueType().getName()</var><var>_locale_identification</var>.properties</kbd>,
+ *   The <kbd><var>_locale_identification</var></kbd> should follow the traditional rules (see {@link ResourceBundle}.
  *   In this file, there should be an entry with as key
  *   <code>label.<var>tag</var></code>, where <code><var>tag</var></code>
- *   is the programmatic string representation of an enumeration type
- *   value as returned by {@link #getAsText()}, for each value returned
- *   by {@link #getTags()}.</p>
+ *   is the programmatic string representation of an enumeration type value as returned by {@link #getAsText()}, for
+ *   each value returned by {@link #getTags()}.</p>
  * <p>The resource bundle is located using the current
  *   {@link #getResourceBundleLoadStrategy() ResourceBundleLoadStrategy}.</p>
  *
  * @author    Jan Dockx
  * @author    PeopleWare n.v.
  */
-public abstract class ResourceBundleBasedEnumerationValueEditor
-    extends AbstractEnumerationValueEditor {
-
-  /*<section name="Meta Information">*/
-  //------------------------------------------------------------------
-
-  /** {@value} */
-  public static final String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
-  /** {@value} */
-  public static final String CVS_DATE = "$Date$"; //$NON-NLS-1$
-  /** {@value} */
-  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
-  /** {@value} */
-  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
-
-  /*</section>*/
-
+@Copyright("2004 - $Date$, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
+public abstract class ResourceBundleBasedEnumerationValueEditor extends AbstractEnumerationValueEditor {
 
   /**
    * The string pre- and postpended to the {@link #getAsText()} String
@@ -69,33 +60,30 @@ public abstract class ResourceBundleBasedEnumerationValueEditor
    *
    * <strong>value = {@value}</strong>
    */
-  public static final String LABEL_NOT_FOUND_TOKEN = "???"; //$NON-NLS-1$
+  public static final String LABEL_NOT_FOUND_TOKEN = "???";
 
   /**
-   * Return the a label from the {@link #getResourceBundleBasename()}
-   * resource bundle, with key {@link #getLabelKey()}. If the resource
-   * bundle is not found, or the key is not found in the resource
-   * bundle, the String
-   * <code>LABEL_NOT_FOUND_TOKEN + getAsText() + LABEL_NOT_FOUND_TOKEN</code>
+   * Return the a label from the {@link #getResourceBundleBasename()} resource bundle, with key
+   * {@link #getLabelKey()}. If the resource bundle is not found, or the key is not found in the resource
+   * bundle, the String <code>LABEL_NOT_FOUND_TOKEN + getAsText() + LABEL_NOT_FOUND_TOKEN</code>
    * is returned.
+   *
+   * @mudo contract
    */
   public final String getLabel() {
-    String result = ResourceBundles.findKeyWithBasename(getResourceBundleBasename(),
-                                                        new String[] {getLabelKey()},
-                                                        getResourceBundleLoadStrategy());
+    String result = findKeyWithBasename(getResourceBundleBasename(),
+                                        new String[] {getLabelKey()},
+                                        getResourceBundleLoadStrategy());
     if (result == null) {
       result = LABEL_NOT_FOUND_TOKEN + getAsText() + LABEL_NOT_FOUND_TOKEN;
     }
     return result;
   }
 
-  /*<property name="localizedMessageResourceBundleLoadStrategy">*/
+  /*<property name="resource bundle load strategy">*/
   //------------------------------------------------------------------
 
-  /**
-   * @basic
-   * @init result instanceof DefaultResourceBundleLoadStrategy;
-   */
+  @Basic(init = @Expression("result instanceof DefaultResourceBundleLoadStrategy"))
   public final ResourceBundleLoadStrategy getResourceBundleLoadStrategy() {
     return $resourceBundleLoadStrategy;
   }
@@ -105,13 +93,12 @@ public abstract class ResourceBundleBasedEnumerationValueEditor
    *            The new resource bundle load strategy.
    * @post      getResourceBundleLoadStrategy() == strategy;
    */
-  public final void setResourceBundleLoadStrategy(
-                        final ResourceBundleLoadStrategy strategy) {
+  @MethodContract(post = @Expression("resourceBundleLoadStrategy == _strategy"))
+  public final void setResourceBundleLoadStrategy(final ResourceBundleLoadStrategy strategy) {
     $resourceBundleLoadStrategy = strategy;
   }
 
-  ResourceBundleLoadStrategy $resourceBundleLoadStrategy =
-      new DefaultResourceBundleLoadStrategy();
+  private ResourceBundleLoadStrategy $resourceBundleLoadStrategy = new DefaultResourceBundleLoadStrategy();
 
   /*</property>*/
 
@@ -120,9 +107,7 @@ public abstract class ResourceBundleBasedEnumerationValueEditor
   /*<property name="resourceBundleBasename">*/
   //------------------------------------------------------------------
 
-  /**
-   * @return    getEnumerationValue().getClass().getName();
-   */
+  @MethodContract(post = @Expression("enumerationValueType.name"))
   public final String getResourceBundleBasename() {
     return getEnumerationValueType().getName();
   }
@@ -134,11 +119,9 @@ public abstract class ResourceBundleBasedEnumerationValueEditor
   /*<property name="labelKey">*/
   //------------------------------------------------------------------
 
-  /**
-   * @return "label." + getAsText();
-   */
+  @MethodContract(post = @Expression("'label.' + asText"))
   public final String getLabelKey() {
-    return "label." + getAsText(); //$NON-NLS-1$
+    return "label." + getAsText();
   }
 
   /*</property>*/

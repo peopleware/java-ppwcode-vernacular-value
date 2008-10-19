e94430dfe2032358f64c6dcdcd050fc3c3615a41
Jan Dockx <jan_dockx@peopleware.be>
2008-10-19 19:50:35 +0000

    vernacular now comes in interfaces
    git-svn-id: file:///repositories/PPWCode/java/vernacular/value/trunk@3194 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ src/main/java/org/ppwcode/vernacular/value_III/MutableValue.java
@@ -0,0 +1,80 @@
+/*<license>
+Copyright 2004 - $Date: 2008-10-01 23:22:38 +0200 (Wed, 01 Oct 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.ppwcode.vernacular.value_III;
+
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
+
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
+import org.toryt.annotations_I.Expression;
+import org.toryt.annotations_I.MethodContract;
+
+
+/**
+ * <p>This interface defines the general modus operandi for mutable value types.</p>
+ * <p>When a mutable value type is used for a bean property, the following idiom is often used:</p>
+ * <pre>
+ *
+ *     &hellip;
+ *
+ *     /*&lt;property name=&quot;<var>propertyName</var>&quot;&gt;&#x2A;/
+ *     //------------------------------------------------------------------
+ *
+ *     &#x40;Basic
+ *     public <var>ImmutableValueType</var> get<var>PropertyName</var>() {
+ *       return (<var>ImmutableValueType</var>)$<var>propertyName</var>.clone();
+ *     }
+ *
+ *     &#x40;MethodContract(post = &#x40;Expression(&quot;<var>propertyName</var> = <var>_propertyName</var>&quot;))
+ *     public void set<var>PropertyName</var>(<var>ImmutableValueType</var> <var>propertyName</var>) {
+ *       $<var>propertyName</var> = (<var>ImmutableValueType</var>)<var>propertyName</var>.clone();
+ *     }
+ *
+ *     private <var>ImmutableValueType</var> $<var>propertyName</var>;
+ *
+ *     /&#x2A;&lt;/property&gt;&#x2A;/
+ *
+ *     &hellip;
+ *
+ * </pre>
+ * <p>Potentially with the addition of validation and type invariants. Note that values are cloned (or copied) when
+ *   they are set or returned to protect encapsulation.</p>
+ * <p>For this reason, mutable value types <strong>must</strong> implement {@link Cloneable}. Since the properties of
+ *   the value are mutable, it is enough to have a default constructor that sets default values internally. Since we
+ *   demand of subtypes that they implement {@link java.io.Serializable}, a default constructor is mandatory.</p>
+ * <p>Like with any value type, {@link #equals(Object)} and {@link #hashCode()} <strong>must</strong> be overridden
+ *   appropriately.</p>
+ *
+ * @author    Jan Dockx
+ * @author    PeopleWare n.v.
+ */
+@Copyright("2004 - $Date: 2008-10-01 23:22:38 +0200 (Wed, 01 Oct 2008) $, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision: 2834 $",
+         date     = "$Date: 2008-10-01 23:22:38 +0200 (Wed, 01 Oct 2008) $")
+public interface MutableValue extends Value, Cloneable {
+
+  @MethodContract(
+    post = {
+      @Expression("result != this"),
+      @Expression("result.equals(this)")
+    }
+  )
+  MutableValue clone();
+
+}
---
+++ src/main/java/org/ppwcode/vernacular/value_III/Value.java
@@ -0,0 +1,80 @@
+/*<license>
+Copyright 2004 - $Date: 2008-09-18 11:16:23 +0200 (Thu, 18 Sep 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.ppwcode.vernacular.value_III;
+
+
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
+
+import java.io.Serializable;
+
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
+import org.toryt.annotations_I.Expression;
+import org.toryt.annotations_I.Invars;
+import org.toryt.annotations_I.MethodContract;
+
+
+/**
+ * <p>A super type for all classes that emulate <em>algebras</em>, i.e., value types.</p>
+ * <p>Value types <strong>must</strong> override {@link #equals(Object)} and {@link #hashCode()}, so that
+ *   different objects that represent the same value are considered equal. Value types <strong>must</strong>
+ *   override {@link #toString()} to return a textual representation of the value they represent, that does
+ *   not mention object identity. Value types <strong>must</strong> be {@link Serializable}, and thus always
+ *   should have a public default constructor.</p>
+ * <p>According to this vernacular, value types <em>should</em> come with a {@link java.beans.PropertyEditor}
+ *   that supports at least a textual interface. Supporting classes in this library offer support for Hibernate
+ *   {@ink org.hibernate.usertype.UserType} classes, JSF {@link javax.faces.convert.Converter} classes,
+ *   and the likes for other technologies, that are based on JavaBeans {@link java.beans.PropertyEditor}
+ *   classes. JavaBeans {@link java.beans.PropertyEditor} classes are used as the basis for all these
+ *   conversions, because they are the oldest conversion technology, and part of the basic Java API.</p>
+ *
+ * @author Jan Dockx
+ * @author PeopleWare n.v.
+ */
+@Copyright("2004 - $Date: 2008-09-18 11:16:23 +0200 (Thu, 18 Sep 2008) $, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision: 2552 $",
+         date     = "$Date: 2008-09-18 11:16:23 +0200 (Thu, 18 Sep 2008) $")
+@Invars(@Expression(value = "for (Value v1) {for (Value v2) {" +
+                              "v1.equals(mv2) ? v1.hashCode() == v2.hashCode()}" +
+                            "}",
+                    note = "just a formalisation of contract described in English in java.lang.Object"))
+public interface Value extends Serializable {
+
+  /**
+   * The empty string.
+   *
+   * <strong>= &quot;&quot;</strong>
+   */
+  public static final String EMPTY = "";
+
+  /**
+   * This method should be overwritten by actual value types with extra conditions that actually do compare internal
+   * values. The following idiom can be used:
+   * <pre>
+   *     &#x40;MethodContract(post = &#x40;Expression(&quot;<var>local conditions</var>&quot))
+   *     public boolean equals(Object other) {
+   *       return super.equals(other) &amp;&amp; <var>local conditions</var>;
+   *     }
+   * </pre>
+   * Remember that the {@link #hashCode()} must be consistent with <code>equals</code>.
+   */
+  @MethodContract(post = @Expression("result ? ((other != null) && (other.class == this.class))"))
+  boolean equals(final Object other);
+
+}

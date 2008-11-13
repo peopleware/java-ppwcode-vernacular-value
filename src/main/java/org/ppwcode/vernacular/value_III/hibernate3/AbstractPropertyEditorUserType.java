cb255bd744b1553c700da62eaa1cf1275b51f4be
Jan Dockx <jan_dockx@peopleware.be>
2008-11-13 15:51:36 +0000

    cleaned; untested, but should work
    git-svn-id: file:///repositories/PPWCode/java/vernacular/value/trunk@3524 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/value_III/hibernate3/AbstractPropertyEditorUserType.java
+++ src/main/java/org/ppwcode/vernacular/value_III/hibernate3/AbstractPropertyEditorUserType.java
@@ -17,15 +17,19 @@ limitations under the License.
 package org.ppwcode.vernacular.value_III.hibernate3;
 
 
+import static java.sql.Types.VARCHAR;
+import static org.hibernate.Hibernate.STRING;
 import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
+import static org.ppwcode.util.reflect_I.CloneHelpers.safeReference;
 import static org.ppwcode.util.reflect_I.TypeHelpers.type;
+import static org.ppwcode.vernacular.exception_II.ProgrammingErrorHelpers.dependency;
+import static org.ppwcode.vernacular.exception_II.ProgrammingErrorHelpers.newAssertionError;
 
 import java.beans.PropertyEditor;
-import java.io.Serializable;
+import java.beans.PropertyEditorManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Types;
 import java.util.Properties;
 
 import org.hibernate.Hibernate;
@@ -35,71 +39,160 @@ import org.hibernate.usertype.UserType;
 import org.ppwcode.metainfo_I.Copyright;
 import org.ppwcode.metainfo_I.License;
 import org.ppwcode.metainfo_I.vcs.SvnInfo;
-import org.ppwcode.vernacular.value_III.MutableValue;
+import org.ppwcode.vernacular.value_III.Value;
 import org.toryt.annotations_I.Basic;
 import org.toryt.annotations_I.Expression;
 import org.toryt.annotations_I.Invars;
 import org.toryt.annotations_I.MethodContract;
+import org.toryt.annotations_I.Throw;
 
 
 /**
- * MUDO UNFINISHED
- *
- * Support class for Hibernate mapping for values that have a {@link PropertyEditor}.
- * Instance of the value type are mapped to a VARCHAR, that contains the
- * {@link PropertyEditor#getAsText() tag} for the type value, as defined by the property editor.
- * This class only works for <em>immutable</em> value types (which should implement
- * {@link org.ppwcode.vernacular.value_III.ImmutableValue}.
- *
- <typedef name="suit" class='EnumUserType'>
-      <param name="enumClassName">com.company.project.Suit</param>
-  </typedef>
-
-  <class ...>
-    <property name='suit' type='suit'/>
-  </class>
-
-  http://www.hibernate.org/272.html
- * @annotation
- * Based on <a href="http://www.hibernate.org/172.html">work by <em>Christian</em> and <em>sethladd</em></a>.
+ * <p>General Hibernate {@link UserType} for any {@link Value} type that has an associated
+ *   {@link PropertyEditor}. Instance of the value type are mapped to a VARCHAR, that contains the
+ *   {@link PropertyEditor#getAsText() tag} for the value, as defined by the property editor.</p>
+ * <p>When instances are constructed with the default constructor, the {@link #returnedClass()}
+ *   and {@link #getPropertyEditor()} are {@code null}, and the user type is not
+ *   {@link #isOperational() operational}. The instance can be configured then using
+ *   {@link #setParameterValues(Properties)}. Here, you can give the fully qualified name of the
+ *   type this instance is intended to support. With this name, the type is loaded, and a
+ *   {@link PropertyEditor} is sought for the type using the {@link PropertyEditorManager}.
+ *   It is an error if the type does not exist or a property editor cannot be found for the type.
+ *   Alternatively, you can supply the type for which the instance should work using
+ *   {@link #AbstractPropertyEditorUserType(Class)}, or you can supply the type and the
+ *   property editor using {@link #AbstractPropertyEditorUserType(Class, PropertyEditor)}.
+ *   You could also create a subtype using these constructors to fill out these properties.</p>
+ * <p>In a Hibernate context, the easiest use is to define the user type using the {@code param}
+ *   tag when definining the property of an entity:</p>
+ * <pre>
+ * ...
+ *   &lt;property name=&quot;<var>propertyName</var>&quot;&gt;
+ *     &lt;type name=&quot;org.ppwcode.vernacular.value_III.hibernate3.PropertyEditorUserType&quot;&gt;
+ *       &lt;param name=&quot;propertyType&quot;&gt;<var>...property.type...</var>&lt;/param&gt;
+ *     &lt;/type&gt;
+ *   &lt;/property&gt;
+ * ...
+ * </pre>
+ * <p>Alternatively, you can define the user type for a property type in the general configuration
+ *   file.</p>
  *
  * @author    Jan Dockx
- * @author    Peopleware n.v.
- *
- * @mudo UNFINISHED
+ * @author    PeopleWare n.v.
  */
 @Copyright("2004 - $Date$, PeopleWare n.v.")
 @License(APACHE_V2)
 @SvnInfo(revision = "$Revision$",
          date     = "$Date$")
-@Invars(@Expression("propertyEditor != null"))
-public abstract class AbstractPropertyEditorUserType implements UserType {
+public class AbstractPropertyEditorUserType extends AbstractValueUserType {
 
-  public void setParameterValues(Properties params) {
-    String enumClassName = params.getProperty("enumClassName");
-    if (enumClassName == null) {
-       throw new MappingException("enumClassName parameter not specified");
-    }
-    try {
-      $valueType = type(enumClassName);
-    }
-    catch (AssertionError aErr) {  // MUDO booo! type really needs to throw exception
-       throw new MappingException("enumClass " + enumClassName + " not found", aErr);
-    }
- }
-
-  private Class<?> $valueType;
+  /*<construction>*/
+  //------------------------------------------------------------------
 
   /**
-   * @post new.getPreopertyEditor() == editor;
+   * An empty instance. Configure using {@link #setParameterValues(Properties)}.
    */
-  @MethodContract(post = @Expression("propertyEditor = _editor"))
-  protected AbstractPropertyEditorUserType(final PropertyEditor editor) {
-    $editor = editor;
+  @MethodContract(post = {
+    @Expression("returnedClass == null"),
+    @Expression("propertyEditor == null")
+  })
+  public AbstractPropertyEditorUserType() {
+    // NOP
   }
 
   /**
-   * An instance of the enumeration type editor of the enumeration
+   * An instance aimed to persists instances of {@code valueType}. The appropriate
+   * {@link #getPropertyEditor() property editor} is retrieved from the {@link PropertyEditorManager}.
+   * If no {@link PropertyEditor} is found, this is an error.
+   */
+  @MethodContract(post = {
+    @Expression("returnedClass == _valueType"),
+    @Expression("propertyEditor == PropertyEditorManager.findEditor(_valueType)")
+  })
+  public AbstractPropertyEditorUserType(Class<? extends Value> valueType) {
+    $valueType = valueType;
+    initPropertyEditor();
+  }
+
+  /**
+   * An instance aimed to persist instances of {@code valueType} using {@code pe}.
+   */
+  @MethodContract(post = {
+    @Expression("returnedClass == _valueType"),
+    @Expression("propertyEditor == _pe")
+  })
+  public AbstractPropertyEditorUserType(Class<? extends Value> valueType, PropertyEditor pe) {
+    $valueType = valueType;
+    $editor = pe;
+  }
+
+  /*</construction>*/
+
+
+
+  /*<section name="initialization">*/
+  //------------------------------------------------------------------
+
+  @MethodContract(post = @Expression("returnedClass() != null && propertyEditor != null"))
+  public final boolean isOperational() {
+    return $valueType != null && $editor != null;
+  }
+
+  /**
+   * Set the type of values we will operate for, as a fully qualified name. The name of the
+   * type we will operate for needs to be given in a property with key &quot;<code>propertyType</code>&quot;.
+   * We also retrieve a {@link #getPropertyEditor() property editor} from the {@link PropertyEditorManager}
+   * for that type.
+   * If a type with the given name cannot be loaded, or we cannot find a {@link PropertyEditor}
+   * for that type, this is an error.
+   */
+  @MethodContract(post =  {
+    @Expression("returnedClass() == type(_params.getProperty('propertyType')"),
+    @Expression("propertyEditor == PropertyEditorManager.findEditor(returnedClass())")
+  })
+  public void setParameterValues(Properties params) {
+    String fqcn = params.getProperty("propertyType");
+    if (fqcn == null) {
+       throw new MappingException("type parameter not specified in Hibernate configuration");
+    }
+    try {
+      $valueType = type(fqcn);
+    }
+    catch (AssertionError aErr) {  // MUDO booo! type really needs to throw exception
+       throw new MappingException("enumClass " + fqcn + " not found", aErr);
+    }
+    initPropertyEditor();
+ }
+
+  /*</section>*/
+
+
+
+
+
+
+  /*<property name="value type">*/
+  //------------------------------------------------------------------
+
+  /**
+   * The class this user type now works.
+   */
+  @Override
+  @Basic
+  public Class<? extends Value> returnedClass() {
+    return $valueType;
+  }
+
+  private Class<? extends Value> $valueType;
+
+  /*</property>*/
+
+
+
+  /*<property name="propertyEditor">*/
+  //------------------------------------------------------------------
+
+  /**
+   * An instance of the value type property editor for the value
    * type this UserType works for.
    */
   @Basic
@@ -107,138 +200,97 @@ public abstract class AbstractPropertyEditorUserType implements UserType {
     return $editor;
   }
 
+  private void initPropertyEditor() {
+    assert dependency($valueType, "$valueType");
+    $editor = PropertyEditorManager.findEditor($valueType);
+    if ($editor == null) {
+      throw newAssertionError("no property editor found for type " + $valueType.getCanonicalName() +
+                              "; please initialize the PropertyEditorManager for this type, or use the " +
+                              "constructor to provide a property editor", null);
+    }
+  }
+
   private PropertyEditor $editor;
 
-  private static final int[] SQL_TYPES = {Types.VARCHAR};
+  /*</property>*/
+
+
+
+  /*<section name="meta">*/
+  //------------------------------------------------------------------
 
   /**
-   * @return {Types.VARCHAR};
+   * Static definition of the VARCHAR column we will write in.
    */
+  @Invars(@Expression("SQL_TYPES == {VARCHAR}"))
+  private static final int[] SQL_TYPES = {VARCHAR};
+
+  @MethodContract(post = @Expression("SQL_TYPES"))
   public final int[] sqlTypes() {
     return SQL_TYPES;
   }
 
+  /*</section>*/
+
+
+
   /**
-   * @return getEnumerationValueEditor().getEnumerationValue();
+   * For value types for which we can use a property editor (simple value types),
+   * a deep copy is the value itself if the value is immutable, and a klone
+   * otherwise. We will return a safe reference, like {@link #replace(Object, Object, Object)}.
    */
-  public Class<?> returnedClass() {
-    return $valueType;
-  }
-
-  @MethodContract(post = @Expression("(_x == null) ? (_y == null) : _x.equals(_y)"))
-  public final boolean equals(final Object x, final Object y) {
-    return (x == null) ? (y == null) : x.equals(y);
-  }
-
-  @MethodContract(post = @Expression("value"))
+  @MethodContract(post = @Expression("safeReference(value)"))
   public final Object deepCopy(final Object value) {
-    return value;
+    return safeReference(value);
   }
 
-  public final boolean isMutable() {
-    return MutableValue.class.isAssignableFrom(returnedClass());
+  /**
+   * Write an instance of the mapped class to a prepared statement.
+   */
+  @MethodContract(
+    pre  = @Expression("propertyEditor != null"),
+    post = @Expression("STRING.nullSafeSet(statement, getPropertyEditor().setValue(_value); getPropertyEditor().getAsText(), index)"),
+    exc  = @Throw(type = HibernateException.class,
+                  cond = @Expression("_value != null && ! returnedClass().isInstance(_value)"))
+  )
+  public final void nullSafeSet(final PreparedStatement statement, final Object value, final int index)
+      throws HibernateException, SQLException {
+    assert dependency(getPropertyEditor(), "propertyEditor");
+    assert dependency(returnedClass(), "returnedClass()");
+    if (value != null && ! returnedClass().isInstance(value)) {
+      throw new HibernateException("this user type can only handle values of type " + returnedClass().getCanonicalName() +
+                                   "; " + value.getClass().getCanonicalName() + " is not supported");
+    }
+    else {
+      try {
+        $editor.setValue(value);
+        String tag = $editor.getAsText(); //  // IllegalArgumentException
+        STRING.nullSafeSet(statement, tag, index);
+      }
+      catch (IllegalArgumentException iaExc) {
+        throw new HibernateException("property editor \"" + $editor + "\" could not transform " + value + " into a String", iaExc);
+      }
+    }
   }
 
   /**
    * Retrieve an instance of the mapped class from a JDBC resultset.
    * Implementors should handle possibility of null values.
-   *
-   * @result  resultSet.wasNull()
-   *            ==> result == null;
-   * @result  !resultSet.wasNull()
-   *            ==> result == the object returned by the property editor for
-   *                          the string in the field with <code>names[0]</code>.
-   * @throws  HibernateException
-   *          Hibernate.STRING.nullSafeGet(resultSet, names[0]);
-   * @throws  SQLException
-   *          Hibernate.STRING.nullSafeGet(resultSet, names[0]);
-   * @throws  SQLException
-   *          resultSet.wasNull();
    */
   public final Object nullSafeGet(final ResultSet resultSet, final String[] names, final Object owner)
-          throws HibernateException, SQLException {
-    Object result = null;
+      throws HibernateException, SQLException {
     String tag = (String)Hibernate.STRING.nullSafeGet(resultSet, names[0]);
-    if (!(resultSet.wasNull())) {
-      getPropertyEditor().setAsText(tag);
-      result = getPropertyEditor().getValue();
+    Object result = null;
+    if (tag != null) {
+      try {
+        $editor.setAsText(tag); // IllegalArgumentException
+        result = $editor.getValue();
+      }
+      catch (IllegalArgumentException iaExc) {
+        throw new HibernateException("property editor \"" + $editor + "\" could not transform " + tag + " into an object", iaExc);
+      }
     }
     return result;
-  }
-
-  /**
-   * Write an instance of the mapped class to a prepared statement.
-   *
-   * @post    value == null
-   *            ==> the parameter at the given index is set to null
-   * @post    value != null
-   *            ==> the parameter at the given index is set to the string
-   *                representation of the given value as returned by the
-   *                enumeration value editor
-   *
-   * @throws  HibernateException
-   *          (value != null)
-              && !returnedClass().getName().equals(value.getClass().getName())
-   * @throws  SQLException
-   *          value == null
-   *          && statement.setNull(index, Types.VARCHAR) throws a SQLException;
-   * @throws  SQLException
-   *          value != null
-   *          && statement.setString(index, getEnumerationValueEditor().getAsText());
-   */
-  public final void nullSafeSet(final PreparedStatement statement, final Object value, final int index)
-      throws HibernateException, SQLException {
-    // make sure the received value is of the right type
-    if ((value != null)
-          && !returnedClass().getName().equals(value.getClass().getName())) {
-      throw new HibernateException("\"" //$NON-NLS-1$
-                                   + ((value == null)
-                                        ? "null" //$NON-NLS-1$
-                                        : value.toString())
-                                   + "\" is not a " //$NON-NLS-1$
-                               + returnedClass().getName());
-    }
-    if (value == null) {
-      statement.setNull(index, Types.VARCHAR);
-    }
-    else {
-      getPropertyEditor().setValue(value);
-      statement.setString(index, getPropertyEditor().getAsText());
-    }
-  }
-
-  public Object assemble(Serializable cached, Object owner) throws HibernateException {
-    // make sure the received value is of the right type
-    if ((cached != null)
-        && !returnedClass().getName().equals(cached.getClass().getName())) {
-      throw new HibernateException("\"" + ((cached == null) ? "null" : cached.toString())
-                                   + "\" is not a " + returnedClass().getName());
-    }
-    return cached;
-  }
-
-  public Serializable disassemble(Object value) throws HibernateException {
-    // make sure the received value is of the right type
-    if ((value != null)
-        && !returnedClass().getName().equals(value.getClass().getName())) {
-      throw new HibernateException("\"" + ((value == null) ? "null" : value.toString())
-                                   + "\" is not a " + returnedClass().getName());
-    }
-    return (Serializable)value; // enumeration values are Serializable
-  }
-
-  public int hashCode(Object x) throws HibernateException {
-    return x.hashCode();
-  }
-
-  public Object replace(Object original, Object target, Object owner) throws HibernateException {
-    // make sure the received value is of the right type
-    if ((original != null)
-        && !returnedClass().getName().equals(original.getClass().getName())) {
-      throw new HibernateException("\"" + ((original == null) ? "null" : original.toString())
-                                   + "\" is not a " + returnedClass().getName());
-    }
-    return original; // super method doc says this is what to do for immutable types, which we are (enum)
   }
 
 }

630e86c2cf25315a754135f9f1dd0c13bca069eb
ppw.bot <ppw.bot@peopleware.be>
2008-11-28 20:00:10 +0000

    svn_spider [r1824]
    working root:
      https://ppwcode.googlecode.com/svn/java/vernacular/value/trunk
    
    fixes:
      src/main/java/org/ppwcode/vernacular/value_III/hibernate3/AbstractImmutableValueCompositeUserType.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/main/java/org/ppwcode/vernacular/value_III/hibernate3/AbstractValueCompositeUserType.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
    
    
    git-svn-id: file:///repositories/PPWCode/java/vernacular/value/trunk@3766 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/value_III/hibernate3/AbstractValueCompositeUserType.java
+++ src/main/java/org/ppwcode/vernacular/value_III/hibernate3/AbstractValueCompositeUserType.java
@@ -1,5 +1,5 @@
 /*<license>
-Copyright 2004 - $Date: 2008-11-12 21:00:43 +0100 (wo, 12 nov 2008) $ by PeopleWare n.v..
+Copyright 2004 - $Date$ by PeopleWare n.v..
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -44,10 +44,10 @@ import org.toryt.annotations_I.MethodContract;
  * @author    Jan Dockx
  * @author    PeopleWare NV
  */
-@Copyright("2008 - $Date: 2008-11-12 21:00:43 +0100 (wo, 12 nov 2008) $, PeopleWare n.v.")
+@Copyright("2008 - $Date$, PeopleWare n.v.")
 @License(APACHE_V2)
-@SvnInfo(revision = "$Revision: 3517 $",
-         date     = "$Date: 2008-11-12 21:00:43 +0100 (wo, 12 nov 2008) $")
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
 public abstract class AbstractValueCompositeUserType implements CompositeUserType, LoggableUserType {
 
   @Basic
